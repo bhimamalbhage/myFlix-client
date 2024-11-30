60fb23867931d4cb8eb46d5f93cac69373b92a94
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-11-29 18:56:14 -0600

    displaying movies using movies api

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import MovieView from "./MovieView";
-
-import inceptionImg from "../img/inception.jpg";
-import titanicImg from "../img/titanic.jpg";
-import matrixImg from "../img/matrix.jpg";
+import PropTypes from "prop-types";
+import axios from "axios";
 
 const MainView = () => {
-  const [movies] = useState([
-    { id: 1, title: "Inception", description: "A mind-bending thriller.", director: "Christopher Nolan", genre: "Sci-Fi", poster: inceptionImg },
-    { id: 2, title: "Titanic", description: "A romantic tragedy.", director: "James Cameron", genre: "Drama", poster: titanicImg },
-    { id: 3, title: "The Matrix", description: "A sci-fi action masterpiece.", director: "The Wachowskis", genre: "Sci-Fi", poster: matrixImg },
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    
+    axios
+      .get("https://movies-flix-bhima-f885454e03b7.herokuapp.com/movies")
+      .then((response) => {
+        console.log("movie response", response);
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
+  }, []);
 
   if (selectedMovie) {
     return <MovieView movie={selectedMovie} onBack={() => setSelectedMovie(null)} />;
@@ -24,11 +30,28 @@ const MainView = () => {
       <h1>MyFlix Movies</h1>
       <div className="movie-list">
         {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} onClick={() => setSelectedMovie(movie)} />
+          <MovieCard key={movie._id} movie={movie} onClick={() => setSelectedMovie(movie)} />
         ))}
       </div>
     </div>
   );
+};
+
+MainView.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      director: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      genre: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      imageUrl: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default MainView;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MovieCard = ({ movie, onClick }) => {
-  return (
-    <div className="movie-card" onClick={onClick} style={{ cursor: "pointer" }}>
-      <h2>{movie.title}</h2>
-    </div>
-  );
+const MovieCard = ({ movie, onClick }) => (
+  <div className="movie-card" onClick={onClick} style={{ cursor: "pointer" }}>
+    <h2>{movie.title}</h2>
+    <img src={movie.imageUrl} alt={`${movie.title} Poster`} style={{ width: "200px" }} />
+  </div>
+);
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default MovieCard;
