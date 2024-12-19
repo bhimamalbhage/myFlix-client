a9a00d6fd6595c3e2a146625d81f1c62308c24df
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-12-19 17:02:04 -0600

    applied bootstrap ui to components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MainView from "./components/main-view/MainView";
 import "./styles.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from "react-bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MainView />);
+
+root.render(
+  <Container fluid>
+    <MainView />
+  </Container>
+);
